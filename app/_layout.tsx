bcdf2dacf69df68bb016c51cc621f17bce2e04f6
Eranga Hansaja <erangahansaja222@gmail.com>
2026-09-15 09:08:58 +0530

    chore: update version to 1.1.0 and configure android navigation bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import { DarkTheme, ThemeProvider } from 'expo-router';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { NavigationBar } from 'expo-navigation-bar';
 
 export default function RootLayout() {
   return (
     <ThemeProvider value={DarkTheme}>
+      <NavigationBar style="dark" />
       <Stack
         screenOptions={{
           headerStyle: {
